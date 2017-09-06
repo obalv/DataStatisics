778c835bea1b369b5eee677bd5062f9ce94f169b
Feature <mwmw654@qq.com>
2017-09-06 18:11:28 +0800

    0.1

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ querylist = {
 	'interfaceId': 'getSurfEleByTimeRangeAndStaID',
 	'dataCode': 'SURF_CHN_MUL_HOR',
 	'dataFormat': 'json',
-	'elements':[] 'Station_Id_C,Station_Id_d,Year,Mon,Day,Hour,TEM,PRE,WIN_D_Avg_2mi,WIN_S_Avg_2mi',
+	'elements': 'Station_Id_C,Year,Mon,Day,PRS_Avg,WIN_S_Inst_Max,WIN_D_INST_Max,TEM_Avg,RHU_Avg,PRE_Time_2020,WEP_Record',
 	'timeRange': '[20141231130000,20151231120000]',
 	'staIds': '57469'
 }
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	res = requests.get(Config.baseUrl, params=querylist)
 	data = json.loads(res)
 	if data['returnCode'] == str(0):
 		for row in data['DS']:
+			with open('weishui.csv', 'w') as f:
+				tablenames = ['区站号', '年', '月', '日', '时', '温度', '降水', '两分钟平均风向', '两分钟平均风速']
+				writer = csv.DictWriter(f, fieldnames=tablenames)
+				writer.writeheader()
